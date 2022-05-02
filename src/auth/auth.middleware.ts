689bf5d879bae3022cec081a6684f68041bfbd29
Francis Masha <francismasha96@gmail.com>
2022-05-02 13:50:26 +0300

    feat: added project api with graphql

---
+++ src/auth/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { get } from 'lodash';
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private authService: AuthService) {}
+
+  async use(
+    req: Request & { user: string; ip: string },
+    res: Response,
+    next: Function,
+  ) {
+    const token = get(req, 'cookies.token');
+    if (!token) {
+      return next();
+    }
+
+    const refreshToken = await this.authService.getSession({ token });
+    req.user = get(refreshToken, 'user', null);
+
+    return next();
+  }
+}
